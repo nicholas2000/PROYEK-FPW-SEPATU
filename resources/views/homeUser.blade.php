ecb6e4a5baa2c1fe73aefeb575b64f4225c03cfa
nicholas2000 <sughapur@gmail.com>
2022-11-21 15:05:51 +0700

    FrontEnd+Validate

---
+++ resources/views/homeUser.blade.php
@@ -0,0 +1,14 @@
+@extends('layoutUser.sideBarUser')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @section('content')
+    <p>Coding Di HomeUser.blade.php</p>
+    @endsection
+</body>
+</html>
